bf46793d5255c25a27708dc7ac09a57c94393945
Shreshth Bhatt <shreshthgit56@gmail.com>
2021-04-30 18:48:45 +0530

    Side Navbar

---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="upperNav">adwdawadas</div>
+            <div className="mainContent">
+                <h1>Uniquely Yours</h1>
+                <div className="cardsWrap">
+                    <div className="card">
+                       <div className="cardImage">
+                           <img src="https://images.unsplash.com/photo-1592966556928-5c4d163e8bd3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVhY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" 
+                           alt="Peace Music"/>
+                        </div> 
+                        <div className="cardContent">
+                            <h3>Liked songs</h3>
+                        </div>
+                    </div>
+                </div>
+
+            </div> 
+        </div>
+    )
+}
+
+export default Main
